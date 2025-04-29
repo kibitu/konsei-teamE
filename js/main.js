01b3ffa136efd176c319a84a5097788df0c81a71
kibitu <134574121+kibitu@users.noreply.github.com>
2025-04-29 14:24:15 +0900

    konsei

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(function(){
+/*-------------------------------
+ドロップダウンメニュー
+---------------------------------*/
+$(".navi .menu .menu-first span").click(function () {
+    $(this).toggleClass("active");
+    $(this).next().slideToggle();
+});
+
+$(".navi .menu .menu-second").click(function () {
+    $(this).prev().toggleClass("active");
+    $(this).slideToggle();
+});
+  /*-------------------------------
+  タブ切り替え
+  ---------------------------------*/
+  $(".tab-list .tab-all").addClass("active");
+  $(".products-list.all").addClass("active");
+
+  $(".tab-all").click(function () {
+    $(".tab-list li").removeClass("active");
+    $(".products-list").removeClass("active");
+    $(this).addClass("active");
+    $(".products-list.all").addClass("active");
+  });
+  
+  $(".tab-sofa").click(function () {
+    $(".tab-list li").removeClass("active");
+    $(".products-list").removeClass("active");
+    $(this).addClass("active");
+    $(".products-list.sofa").addClass("active");
+  });
+  
+  $(".tab-desk").click(function () {
+    $(".tab-list li").removeClass("active");
+    $(".products-list").removeClass("active");
+    $(this).addClass("active");
+    $(".products-list.desk").addClass("active");
+  });
+  
+  // $(".tab-chair").click(function () {
+  //   $(".tab-list li").removeClass("active");
+  //   $(".products-list").removeClass("active");
+  //   $(this).addClass("active");
+  //   $(".products-list.chair").addClass("active");
+  // });
+  
+  // $(".tab-dining").click(function () {
+  //   $(".tab-list li").removeClass("active");
+  //   $(".products-list").removeClass("active");
+  //   $(this).addClass("active");
+  //   $(".products-list.dining").addClass("active");
+  // });
+
+});
+
+
