ae6c2ad8a0bd1535818df982a92f5274d4499c68
kibitu <134574121+kibitu@users.noreply.github.com>
2025-05-12 21:13:19 +0900

    Add files via upload

---
+++ Shiborikonsei_About/about.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+    <meta http-equiv="content-type" charset="utf-8"><link rel="stylesheet" href="css/stylesheet.css">
+    <title>会社概要</title>
+</head>
+<body>
+    <?php include "include/header.php"; ?>
+    <main>
+        <div id="profile_width" class="profile">
+            <div class="title_field">
+                <h1 class="title">会社概要</h1>
+            </div>
+            <div class="profile_content">
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>会社名</th>
+                            <td>有限会社こんせい</td>
+                        </tr>
+                        <tr>
+                            <th>所在地</th>
+                            <td>愛知県名古屋市緑区鳴海町字下中21番地</td>
+                        </tr>
+                        <tr>
+                            <th>電話番号</th>
+                            <td>052-624-0029</td>
+                        </tr>
+                        <tr>
+                            <th>FAX</th>
+                            <td>052-622-6182</td>
+                        </tr>
+                        <tr>
+                            <th>設立</th>
+                            <td>平成24年1月</td>
+                        </tr>
+                        <tr>
+                            <th valign="top" class="content_Adjust">代表</th>
+                            <td class="content_Adjust">
+                                近藤 泰仁
+                                <ul>
+                                    <li>中日文化センター生涯学習センター講師</li>
+                                    <li>トヨタ産業技術記念館週末ワークショップ講師</li>
+                                    <li>小学校や幼稚園など絞り染めの授業年間20校ほど</li>
+                                    <li>
+                                        名古屋市姉妹友好都市にて<br>
+                                        現地交流イベントでのワークショップ （イタリア・アメリカ）
+                                    </li>
+                                    <li>台湾などでのワークショップを含んだ交流活動</li>
+                                    <ul class="contents_sub">
+                                        <li>片桐銘木工業株式会社様と<br>愛知県の木に絞り染めをする取組</li>
+                                        <li>
+                                            株式会社スクウェア・エニックス様と<br>
+                                            ゲーム内キャラクターの衣装作製（雪花絞り浴衣）
+                                        </li>
+                                    </ul>
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th class="content_Adjust">事業内容</th>
+                            <td class="content_Adjust">
+                                伝統工芸鳴海絞り、刺繍手加工品婦人服や<br>
+                                雑貨類の製造・販売、絞り染めのワークショップ
+                            </td>
+                        </tr>
+                        <tr>
+                            <th valign="top" class="content_Adjust">沿革</th>
+                            <td class="content_Adjust">
+                                <ul class="contents">
+                                    <li>
+                                        <h3>大正 6年（1916）</h3>
+                                        <p>近藤泰仁の祖父近藤清一が個人創業</p>
+                                    </li>
+                                    <li>
+                                        <h3>昭和26年（1951）</h3>
+                                        <p>有限会社 近清商店 設立</p>
+                                    </li>
+                                    <li>
+                                        <h3>昭和46年（1971）</h3>
+                                        <p>近藤泰仁の父近藤 典親社長就任</p>
+                                    </li>
+                                    <li>
+                                        <h3>平成6年（1994）</h3>
+                                        <p>有限会社 近清商店に入社</p>
+                                    </li>
+                                    <li>
+                                        <h3>平成24年（2012）</h3>
+                                        <p>有限会社こんせい設立 社長就任</p>
+                                    </li>
+                                </ul>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th valign="top" class="content_Adjust">駐車場案内図</th>
+                            <td class="content_Adjust"><img src="images/parking.jpg" alt="駐車場案内図"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+    <footer>shiborikonsei-e 仮フッタ</footer>
+    <script src="js/profile.js"></script>
+    <script src="js/jquery-3.7.1.min.js"></script>
+</body>
+</html>
